2816331a22c89467f5de31dd4777c2e815b0d1e2
Alexander <32795918+Alexeri@users.noreply.github.com>
2025-08-04 15:45:18 +0200

    hardcoded students

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,7 @@
 "use strict";
+const students = [
+    { name: "Alice" },
+    { name: "Bob" },
+    { name: "Charlie" },
+];
 //# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+interface Student {
+  name: string;
+}
+const students: Student[] = [
+  { name: "Alice" },
+  { name: "Bob" },
+  { name: "Charlie" },
+];
